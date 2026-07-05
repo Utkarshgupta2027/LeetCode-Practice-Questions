58fddfc267041105f4629c197fbdf128aab82289
Utkarsh Gupta <gutkarsh702@gmail.com>
2026-07-05 13:28:13 +0530

    Refactor FibonacciSeries class to use a recursive method for Fibonacci calculation

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -2,28 +2,32 @@
 import java.util.Scanner;
 
 public class FibonacciSeries {
-    // public  int fib(int n){
+    public static  int fib(int n){
         
-    //     if(n == 0 || n == 1) return n;
+        if(n == 0 || n == 1) return n;
 
-    //     return 
-    // }
+        return fib(n - 1) + fib(n - 2);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter number: ");
         int n = sc.nextInt();
-        int a = 0, b = 1, i = 0;
+      //  int a = 0, b = 1, i = 0;
 
-        while(i <= n){
-            System.out.print(a + " ");
-            int c = a + b;
+        // while(i <= n){
+        //     System.out.print(a + " ");
+        //     int c = a + b;
 
-            a = b;
-            b = c;
+        //     a = b;
+        //     b = c;
 
-            i++;
-        }
+        //     i++;
+        // }
+        
+    for(int i = 0; i < n; i++){
+        System.out.print(fib(i) + " ");
+    }
         sc.close();
     }
 }
